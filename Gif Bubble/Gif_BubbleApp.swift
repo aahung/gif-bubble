8e5e71655efb9e12dc1bb2851c1f1446e294a5a0
_sam <3804518+aahung@users.noreply.github.com>
2021-07-09 22:03:42 -0700

    Initial Commit

---
+++ Gif Bubble/Gif_BubbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gif_BubbleApp.swift
+//  Gif Bubble
+//
+//  Created by Xinhong LIU on 7/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Gif_BubbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
